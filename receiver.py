dfdc1464b01d704385e3d7ea27cb90fefcb0b10f
Talita Bernardes Pereira <talita.bernardesp@gmail.com>
2022-11-17 09:57:17 -0300

    Update receiver.py

--- receiver.py
+++ receiver.py
@@ -4,7 +4,7 @@ import boto3
 def consume_queue():
     sqs_client = boto3.client('sqs', region_name='us-east-1')
     response = sqs_client.receive_message(
-        QueueUrl="https://sqs.us-east-1.amazonaws.com/932670009615/letscode-app",
+        QueueUrl="https://sqs.us-east-1.amazonaws.com/account/letscode-app",
         MaxNumberOfMessages=10,
         WaitTimeSeconds=10
     )
